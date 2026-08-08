3644fd222bb3e609d4da823b052f118710a65fb6
NomadsReach <144523850+NomadsReach@users.noreply.github.com>
2026-08-08 16:48:11 -0400

    add discord button and redirect /prismaui-f4 to new docs
    - update discord invite link in header to bawdketrFX
    - add static redirect: /prismaui-f4 -> prisma-user-interface-framework.github.io/Prisma2.0/docs/getting-started
      so existing nexus mod links keep working

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,9 +24,14 @@ function remarkAdmonitions() {
   };
 }
 
+const PRISMA_DOCS = 'https://prisma-user-interface-framework.github.io/Prisma2.0/docs/getting-started';
+
 export default defineConfig({
   site: 'https://fallenworld.nexus',
   base: '/',
+  redirects: {
+    '/prismaui-f4': PRISMA_DOCS,
+  },
   integrations: [
     mdx({
       remarkPlugins: [remarkDirective, remarkAdmonitions],
